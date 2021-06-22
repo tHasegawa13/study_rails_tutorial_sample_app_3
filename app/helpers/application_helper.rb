4b7bcbefe4e0b599456197b87c731989a007aba1
tHasegawa13 <thsgw13.stac@gmail.com>
2021-06-22 03:07:33 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def full_title(title)
-    "#{title} | Ruby on Rails Tutorial Sample App"
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
   end
 end
